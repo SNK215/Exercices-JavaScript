6260756ab22c7a71e3491c60d93fde9b361e6154
SNK215 <langowski.lucas01@gmail.com>
2023-01-11 09:22:33 +0100

    modification exo9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,19 +1,23 @@
 let age = parseInt(prompt("Entrez l'âge du salarié: "));
 let anciennete = parseInt(prompt("Entrez l'ancienneté du salarié: "));
-let dernierSalaire = parseInt(prompt("Entrez le dernier salaire du salarié: ")); 
+let salaire = parseInt(prompt("Entrez le dernier salaire du salarié: ")); 
+let indemnite;
 
 if (anciennete <= 10) {
 
     console.log("Le salarié touche une indémnité équivalente à la moitié d'un salaire");
+    indemnite = salaire/2;
     if (age > 45) {
     
         if (age >= 46 && age <= 49) {
             
-            console.log("Le salarié touche une indémnité supplémentaire équivalente à 2 mois de salaire");
+            indemnite = indemnite + 2*salaire
+            console.log(`Le salarié touche une indémnité supplémentaire équivalente à 2 mois de salaire (total: ${indemnite}€)`);
     
         } if (age >= 50) {
-            
-            console.log("Le salarié touche une indémnité supplémentaire équivalente à 5 mois de salaire");
+
+            indemnite = indemnite + 5*salaire;
+            console.log(`Le salarié touche une indémnité supplémentaire équivalente à 5 mois de salaire (total: ${indemnite}€)`);
     
         }
     
@@ -21,16 +25,19 @@ if (anciennete <= 10) {
 
 } if (anciennete > 10) {
 
+        indemnite = salaire;
         console.log("Le salarié touche une indémnité équivalente à un salaire");
         if (age > 45) {
     
             if (age >= 46 && age <= 49) {
-                
-                console.log("Le salarié touche une indémnité supplémentaire équivalente à 2 mois de salaire");
+            
+                indemnite = indemnite + 2*salaire
+                console.log(`Le salarié touche une indémnité supplémentaire équivalente à 2 mois de salaire (total: ${indemnite}€)`);
         
             } if (age >= 50) {
                 
-                console.log("Le salarié touche une indémnité supplémentaire équivalente à 5 mois de salaire");
+                indemnite = indemnite + 5*salaire;
+                console.log(`Le salarié touche une indémnité supplémentaire équivalente à 5 mois de salaire (total: ${indemnite}€)`);
         
             }
         
