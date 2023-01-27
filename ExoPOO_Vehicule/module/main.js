ba450f18197b9a49c5e673b8fcf52c31544c99e4
SNK215 <langowski.lucas01@gmail.com>
2023-01-27 11:30:48 +0100

    exo POO

--- ExoPOO_Vehicule/module/main.js
+++ ExoPOO_Vehicule/module/main.js
@@ -8,3 +8,7 @@ let moto1 = new moto("KTM", "390RC", 32000, 2012);
 console.log(voiture1.display());
 console.log(moto1.display());
 
+document.querySelector("#displayVoiture").textContent = voiture1.display();
+document.querySelector("#displayMoto").textContent = moto1.display();
+
+
